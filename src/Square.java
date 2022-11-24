7ba96ebd0c9d52470f7a276de6d5418dd231b087
Matias <matias.brizuela@davinci.edu.ar>
2022-11-24 10:24:16 -0300

    feat: creamos constructores, getters y setters, y atributos

--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,17 @@
-public class Square {
+public class Square extends Rectangle {
+    public double side;
+    Square(){
+    }
+    Square(double side){
+    }
+    Square(double side, String color, boolean lleno){
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
 }
