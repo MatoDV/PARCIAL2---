32eedeebc7e5b4a4b58a22efdd4c9c07594f924b
Matias <matias.brizuela@davinci.edu.ar>
2022-11-24 10:24:42 -0300

    feat: creamos atributos, constructores, getters y setters

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,17 @@
-public class Circle {
+public class Circle extends Shape{
+    public double radio;
+    Circle(){
+    }
+    Circle(double radio){
+    }
+    Circle(double radio,String color,boolean lleno){
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
 }
