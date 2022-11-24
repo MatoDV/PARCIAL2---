a6569112d3f69e358628e032a96e365e038252d7
Matias <matias.brizuela@davinci.edu.ar>
2022-11-24 10:34:58 -0300

    feat: creamos objetos de circulo cuadrado y rectangulo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+    Circle circulo = new Circle(5,"red",true);
+    Rectangle rectangulo = new Rectangle(4,7,"red",true);
+    Square cuadrado = new Square(5,"red",true);
     }
 }
