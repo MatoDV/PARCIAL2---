ea0a4dc08e3c254290b18a3ff76659439c6e99da
Matias <matias.brizuela@davinci.edu.ar>
2022-11-24 10:24:37 -0300

    feat: creamos atributos, constructores, getters y setters

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,26 @@
-public class Rectangle {
+public class Rectangle extends Shape{
+    public double ancho;
+    public double largo;
+    Rectangle(){
+    }
+    Rectangle(double ancho, double largo){
+    }
+    Rectangle(double ancho, double largo, String color, boolean lleno){
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+    public double getLargo() {
+        return largo;
+    }
+
+    public void setLargo(double largo) {
+        this.largo = largo;
+    }
 }
