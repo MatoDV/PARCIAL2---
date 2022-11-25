2e94a160654d5795d86e9f285921663d112063ae
Matias Brizuela <matias.brizuela@davinci.edu.ar>
2022-11-24 21:41:08 -0300

    feat: nuevos objetos

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,20 @@ public class Main {
 
     Shape cuadrado = new Square(1.0,"red",true);
 
+        Shape s1 = new Circle(5.5, "red", false);
+        //Shape s2 = new Shape();
+        Shape s3 = new Rectangle(1.0, 2.0, "red", false);
+        Shape s4 = new Square(6.6);
+
+
 
         System.out.println(circulo);
         System.out.println(rectangulo);
         System.out.println(cuadrado);
 
-
+        System.out.println(s1);
+        //System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
     }
 }
